fbb06267249a59b7a3aad3fa372636268fa1ceb6
Sakib Ahmed <sakibahmad99090@gmail.com>
2021-06-26 11:23:48 +0600

    added

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.get("/getBookSeat/:id", (req,res) =>  {
     })
 })
 
-app.delete("delete/:id",(req,res) => {
+app.delete("/delete/:id",(req,res) => {
     seatCollection.deleteOne({_id:ObjectId(req.params.id)}).then(result => {
      res.send(result.deletedCount > 0)
     })
